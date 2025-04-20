2d1022ae065d56d1aea181bc7e8728866cf9c48c
AnkitKumarHub <akkimessi710@gmail.com>
2025-04-20 23:39:07 +0530

    Basic setup for the UI

---
+++ README.md
@@ -0,0 +1,7 @@
+# DevTinder
+
+- Create a Vite + React application
+- Remove unnecessary code and create a Hello World app
+- Install tailwind
+- Install daisyUI
+- Add navbar component to app.jsx
---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import NavBar from "./components/NavBar";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import { Provider } from "react-redux";
+import reduxStore from "./utils/reduxStore";
+import Feed from "./components/Feed";
+import Connections from "./components/Connections";
+import Requests from "./components/Requests";
+
+function App() {
+  return (
+    <>
+    <Provider store = {reduxStore}>
+      <BrowserRouter basename = "/">
+        <Routes>
+          <Route path = "/" element = {<Body />}>
+            <Route path = "/" element = {<Feed />} />
+            {/* <Route path = "/login" element = {<Login />} />
+            <Route path = "/profile" element = {<Profile />} />
+            <Route path = "/connections" element = {<Connections />} />
+            <Route path = "/requests" element = {<Requests />} /> */}
+          </Route>
+          {/* // <Route path = "/login" element = {<div>login page  </div>} /> */}
+          {/* // <Route path = "/test" element = {<div>Test page</div>} /> */}
+        </Routes>
+
+      </BrowserRouter>
+      {/* <NavBar /> */}
+      </Provider>
+    </>
+  );
+}
+
+export default App;
