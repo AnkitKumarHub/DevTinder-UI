1d749925606daf4c2ef3c95fc99963c3e34d2c37
AnkitKumarHub <akkimessi710@gmail.com>
2025-04-22 09:17:18 +0530

    feat: signUp & requests components

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,24 +1,24 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useDispatch } from 'react-redux';
-import { addUser } from '../utils/userSlice';
-import { useNavigate } from 'react-router-dom';
-import { BASE_URL } from '../utils/constants';
+import React, { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import { BASE_URL } from "../utils/constants";
 
 const Login = () => {
-  const [emailId, setEmailId] = useState('ankit@gmail.com');
-  const [password, setPassword] = useState('Ankit@123');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [emailId, setEmailId] = useState();
+  const [password, setPassword] = useState();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [isLoginForm, setIsLoginForm] = useState(true);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleLogin = async () => {
     try {
       const res = await axios.post(
-        BASE_URL + '/login',
+        BASE_URL + "/login",
         {
           emailId,
           password,
@@ -29,26 +29,26 @@ const Login = () => {
       // console.log(res.data); //res.data contains the actual data i.e, user data which we sent through api in backend => store this data in redux store => useDispatch hook
       dispatch(addUser(res.data));
 
-      return navigate('/'); //navigate to this path after clicking login
+      return navigate("/"); //navigate to this path after clicking login
     } catch (err) {
-      setError(err?.response?.data || 'Something Went Wrong');
+      setError(err?.response?.data || "Something Went Wrong");
       // console.error(err);
     }
   };
 
-  const handleSignUp = async() => {
-    try{
-
-      const res = await axios.post(BASE_URL + "/signup", {firstName, lastName, emailId, password},
-        {withCredentials: true}
-      )
+  const handleSignUp = async () => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/signup",
+        { firstName, lastName, emailId, password },
+        { withCredentials: true }
+      );
 
       dispatch(addUser(res.data.data));
 
       return navigate("/profile");
-
-    } catch(err) {
-      setError(err?.response?.data || 'Something Went Wrong');
+    } catch (err) {
+      setError(err?.response?.data || "Something Went Wrong");
       console.error(err);
     }
   };
@@ -57,68 +57,70 @@ const Login = () => {
     <div className="flex justify-center my-10">
       <div className="card bg-base-300 w-96 shadow-sm">
         <div className="card-body">
-          <h1 className="card-title flex justify-center my-2">{isLoginForm ? "LOGIN" : "SIGN UP"}</h1>
+          <h1 className="card-title flex justify-center my-2">
+            {isLoginForm ? "LOGIN" : "SIGN UP"}
+          </h1>
 
-          {(!isLoginForm) && (
+          {!isLoginForm && (
             <>
-            <label className="input">
-            <svg
-              className="h-[1em] opacity-50"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
-              >
-                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-              </g>
-            </svg>
-            <input
-              type="text"
-              placeholder="Firstname"
-              required
-              value={firstName}
-              onChange={e => {
-                setFirstName(e.target.value);
-                setError("");// Clear the error when user starts typing again
-              }}
-            />
-          </label>
-          <label className="input">
-            <svg
-              className="h-[1em] opacity-50"
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-            >
-              <g
-                strokeLinejoin="round"
-                strokeLinecap="round"
-                strokeWidth="2.5"
-                fill="none"
-                stroke="currentColor"
-              >
-                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
-                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
-              </g>
-            </svg>
-            <input
-              type="text"
-              placeholder="Lastname"
-              required
-              value={lastName}
-              onChange={e => {
-                setLastName(e.target.value);
-                setError("");// Clear the error when user starts typing again
-              }}
-            />
-          </label>
-           </>)
-          }
+              <label className="input">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                  </g>
+                </svg>
+                <input
+                  type="text"
+                  placeholder="Firstname"
+                  required
+                  value={firstName}
+                  onChange={(e) => {
+                    setFirstName(e.target.value);
+                    setError(""); // Clear the error when user starts typing again
+                  }}
+                />
+              </label>
+              <label className="input">
+                <svg
+                  className="h-[1em] opacity-50"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                >
+                  <g
+                    strokeLinejoin="round"
+                    strokeLinecap="round"
+                    strokeWidth="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                  >
+                    <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                    <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+                  </g>
+                </svg>
+                <input
+                  type="text"
+                  placeholder="Lastname"
+                  required
+                  value={lastName}
+                  onChange={(e) => {
+                    setLastName(e.target.value);
+                    setError(""); // Clear the error when user starts typing again
+                  }}
+                />
+              </label>
+            </>
+          )}
           <label className="input">
             <svg
               className="h-[1em] opacity-50"
@@ -141,9 +143,9 @@ const Login = () => {
               placeholder="mail@site.com"
               required
               value={emailId}
-              onChange={e => {
+              onChange={(e) => {
                 setEmailId(e.target.value);
-                setError("");// Clear the error when user starts typing again
+                setError(""); // Clear the error when user starts typing again
               }}
             />
           </label>
@@ -177,7 +179,7 @@ const Login = () => {
               value={password}
               onChange={(e) => {
                 setPassword(e.target.value);
-                setError("");// Clear the error when user starts typing again
+                setError(""); // Clear the error when user starts typing again
               }}
             />
           </label>
@@ -196,28 +198,31 @@ const Login = () => {
             <p className="text-red-500 text-center text-sm ">{error}</p>
           )}
           <div className="card-actions justify-center my-3">
-            <button className="btn btn-primary" onClick={() => { if (isLoginForm) {
-    handleLogin();
-  } else {
-    handleSignUp();
-  }}}>
+            <button
+              className="btn btn-primary"
+              onClick={() => {
+                if (isLoginForm) {
+                  handleLogin();
+                } else {
+                  handleSignUp();
+                }
+              }}
+            >
               {isLoginForm ? "Login" : "Sign Up"}
             </button>
           </div>
 
           <p className="text-center mt-3 text-sm">
-  <span>
-    {isLoginForm ? 'New User? ' : 'Existing User? '}
-    <button 
-      className="text-blue-500 hover:underline hover:text-blue-600 transition-all duration-200 cursor-pointer"
-      onClick={() => setIsLoginForm(value => !value)}
-    >
-      {isLoginForm ? 'Signup Here' : 'Login Here'}
-    </button>
-  </span>
-</p>
-
-
+            <span>
+              {isLoginForm ? "New User? " : "Existing User? "}
+              <button
+                className="text-blue-500 hover:underline hover:text-blue-600 transition-all duration-200 cursor-pointer"
+                onClick={() => setIsLoginForm((value) => !value)}
+              >
+                {isLoginForm ? "Signup Here" : "Login Here"}
+              </button>
+            </span>
+          </p>
         </div>
       </div>
     </div>
---
+++ src/components/Requests.jsx
@@ -0,0 +1,119 @@
+import axios from "axios";
+import React, { useEffect } from "react";
+import { BASE_URL } from "../utils/constants";
+import { useDispatch, useSelector } from "react-redux";
+import { addRequest, removeRequest } from "../utils/requestSlice";
+
+const Requests = () => {
+  const requests = useSelector((store) => store.requests);
+
+  const dispatch = useDispatch();
+
+  const reviewRequest = async (status, requestId) => {
+    try {
+      await axios.post(
+        BASE_URL + "/request/review/" + status + "/" + requestId,
+        {
+          //empty => as this is a post call and i don't have to send any data
+        },
+        { withCredentials: true }
+      );
+
+      dispatch(removeRequest(requestId));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+  const fetchRequests = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/user/requests/recieved", {
+        withCredentials: true,
+      });
+      // console.log(res?.data);
+      dispatch(addRequest(res?.data?.connectionRequests));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchRequests();
+  }, []);
+
+  if (!requests) return null;
+
+  if (requests.length == 0) {
+    return (
+      <div className="text-center my-10">
+        <h1 className="text-4xl font-bold text-gray-500">No Requests Found</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="my-10">
+      <h1 className="text-4xl font-bold text-primary-content text-center tracking-wide mb-6">
+        Requests
+      </h1>
+      <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-1 gap-6">
+        {requests.map((request) => {
+          const { firstName, lastName, photoUrl, age, gender, about } =
+            request.fromUserId;
+          return (
+            <div key={firstName + lastName} className="flex justify-center">
+              {" "}
+              {/* if key is not added it gives error => always use key when mapping over an array*/}
+              <div className="w-full max-w-md bg-gradient-to-br from-[#1f2937] to-[#111827] text-white shadow-xl rounded-lg overflow-hidden transform hover:scale-105 transition duration-300">
+                <div className="flex p-6">
+                  {/* Profile Image */}
+                  <div className="w-24 h-24 rounded-full overflow-hidden bg-gray-300 mr-6">
+                    <img
+                      className="w-full h-full object-cover"
+                      src={photoUrl || "https://via.placeholder.com/150"}
+                      alt="User Avatar"
+                    />
+                  </div>
+
+                  {/* Text Content */}
+                  <div className="flex-1">
+                    <h2 className="text-2xl font-semibold capitalize">
+                      {firstName + " " + lastName}
+                    </h2>
+                    <div className="flex justify-start mt-2 text-sm text-gray-300">
+                      {age && gender && (
+                        <p>
+                          {age} years old, {gender}
+                        </p>
+                      )}
+                    </div>
+                    <p className="mt-3 text-sm text-gray-300">
+                      {about || "No description available."}
+                    </p>
+
+                    {/* Buttons */}
+                    <div className="flex gap-3 mt-4">
+                      <button
+                        onClick={() => reviewRequest("accepted", request._id)}
+                        className="btn btn-secondary"
+                      >
+                        Accept
+                      </button>
+                      <button
+                        onClick={() => reviewRequest("rejected", request._id)}
+                        className="btn btn-primary"
+                      >
+                        Reject
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Requests;
